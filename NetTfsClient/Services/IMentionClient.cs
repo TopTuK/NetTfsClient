54b25598f500e51d0237610495735afa11a5dc07
Sergey <cydoor88@gmail.com>
2023-03-11 13:55:47 +0300

    add Mention client

---
+++ NetTfsClient/Services/IMentionClient.cs
@@ -0,0 +1,35 @@
+﻿using NetTfsClient.Models.Project;
+using NetTfsClient.Models.Workitems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetTfsClient.Services
+{
+    public enum MentionResult : byte
+    {
+        ERROR = 0,
+        SUCCESS
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IMentionClient
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="workitem"></param>
+        /// <param name="toUser"></param>
+        /// <param name="title"></param>
+        /// <param name="message"></param>
+        /// <param name="fromUser"></param>
+        /// <returns></returns>
+        Task<MentionResult> SendMentionAsync(IWorkitem workitem, 
+            ITeamMember toUser, string title, string message, 
+            ITeamMember? fromUser = null);
+    }
+}
---
+++ NetTfsClient/Services/MentionClient/MentionClient.cs
@@ -0,0 +1,48 @@
+﻿using NetTfsClient.Models;
+using NetTfsClient.Models.Project;
+using NetTfsClient.Models.Workitems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetTfsClient.Services.MentionClient
+{
+    internal class MentionClient : IMentionClient
+    {
+        private static string TFS_HISTORY_FIELD = "System.History";
+
+        public async Task<MentionResult> SendMentionAsync(IWorkitem workitem, 
+            ITeamMember toUser, string title, string message, ITeamMember? fromUser = null)
+        {
+            StringBuilder mention = new StringBuilder();
+
+            // Add mention (no public API)
+            mention.AppendLine($"<a href=\"#\" data-vss-mention=\"version:2.0,{toUser.Id}\">@{toUser.DisplayName}</a><br>");
+
+            // Add comment/message
+            mention.AppendLine(message);
+
+            // Ignore if user sends mention himself
+            if ((fromUser != null) && (fromUser.Id != toUser.Id))
+            {
+                mention.AppendLine($"<br>CC: <a href=\"#\" data-vss-mention=\"version:2.0,{fromUser.Id}\">@{fromUser.DisplayName}</a>");
+            }
+
+            try
+            {
+                workitem[TFS_HISTORY_FIELD] = mention.ToString();
+                var updateResult = await workitem.SaveFieldsChangesAsync();
+
+                return (updateResult == UpdateFieldsResult.UPDATE_SUCCESS)
+                    ? MentionResult.SUCCESS
+                    : MentionResult.ERROR;
+            }
+            catch (Exception ex)
+            {
+                throw new TfsClientException("IMentionClient::SendMentionAsync: exception raised", ex);
+            }
+        }
+    }
+}
