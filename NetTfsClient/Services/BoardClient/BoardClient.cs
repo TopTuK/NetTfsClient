dfff49b29c628ed0820dd05cbd86208e67849c32
Sergey <cydoor88@gmail.com>
2024-05-04 12:53:59 +0300

    Add BoardClient

---
+++ NetTfsClient/Services/BoardClient/BoardClient.cs
@@ -0,0 +1,78 @@
+﻿using NetTfsClient.Models;
+using NetTfsClient.Models.Boards;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetTfsClient.Services.BoardClient
+{
+    internal class BoardClient : BaseClient, IBoardClient
+    {
+        private const string BOARDS_URL = @"boards";
+
+        public BoardClient(IClientConnection connection)
+            : base(connection)
+        { 
+        }
+
+        // https://learn.microsoft.com/en-us/rest/api/azure/devops/work/boards/get?view=azure-devops-rest-6.0
+        public async Task<IEnumerable<IBaseBoard>> GetBoardsAsync(string projectId, string teamId)
+        {
+            var requestUrl = $"{clientConnection.CollectionName}/{projectId}/{teamId}/_apis/work/{BOARDS_URL}";
+
+            var queryParams = new Dictionary<string, string>
+            {
+                { "api-version", API_VERSION },
+            };
+
+            try
+            {
+                var httpResponse = await httpClient.GetAsync(requestUrl, queryParams);
+
+                if ((httpResponse == null) || (httpResponse.HasError) || (httpResponse.Content == null))
+                {
+                    throw new ClientException("IBoardClient::GetBoardsAsync: HTTP response is empty or null");
+                }
+
+                return BoardItemsFactory.BoardsFromJsonContent(httpResponse.Content);
+            }
+            catch (Exception ex)
+            {
+                throw new ClientException("IBoardClient::GetBoardsAsync: exception raised", ex);
+            }
+        }
+
+        public async Task<IBoard?> GetBoardAsync(string projectId, string teamId, string boardId)
+        {
+            if (boardId == string.Empty)
+            {
+                throw new ArgumentException("Board Id can\'t be empty string", nameof(boardId));
+            }
+
+            var requestUrl = $"{clientConnection.CollectionName}/{projectId}/{teamId}/_apis/work/{BOARDS_URL}/{boardId}";
+
+            var queryParams = new Dictionary<string, string>
+            {
+                { "api-version", API_VERSION },
+            };
+
+            try
+            {
+                var httpResponse = await httpClient.GetAsync(requestUrl, queryParams);
+
+                if ((httpResponse == null) || (httpResponse.HasError) || (httpResponse.Content == null))
+                {
+                    throw new ClientException("IBoardClient::GetBoardAsync: HTTP response is empty or null");
+                }
+
+                return BoardItemsFactory.BoardFromJsonContent(httpResponse.Content);
+            }
+            catch (Exception ex)
+            {
+                throw new ClientException("IBoardClient::GetBoardAsync: exception raised", ex);
+            }
+        }
+    }
+}
---
+++ TfsClient.Tests/BoardTests/BoardClientTests.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.Extensions.Configuration;
+using NetTfsClient.Models.Boards;
+using NetTfsClient.Models.Project;
+using NetTfsClient.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using TfsClient.Tests.WorkitemsTests;
+
+namespace TfsClient.Tests.BoardTests
+{
+    public class BoardClientTests
+    {
+        private readonly IBoardClient _boardClient;
+        private readonly IProjectClient _prjClient;
+
+        private static void SetEnvironmentVariablesFromUserSecrets()
+        {
+            var configuration = new ConfigurationBuilder()
+               .AddUserSecrets<WorkitemClientTests>()
+               .Build();
+
+            foreach (var child in configuration.GetChildren())
+            {
+                Environment.SetEnvironmentVariable(child.Key, child.Value);
+            }
+        }
+
+        public BoardClientTests()
+        {
+            SetEnvironmentVariablesFromUserSecrets();
+
+            string serverUrl = Environment.GetEnvironmentVariable("ENV_SERVER_URL")!;
+            string projectName = Environment.GetEnvironmentVariable("ENV_PROJECT_NAME")!;
+            string pat = Environment.GetEnvironmentVariable("ENV_PAT")!;
+
+            var clientConnection = ClientFactory.CreateClientConnection(serverUrl, projectName, pat);
+            _boardClient = clientConnection.GetBoardClient();
+            _prjClient = clientConnection.GetProjectClient();
+        }
+
+        private async Task<IProject?> GetProject()
+        {
+            var projects = await _prjClient.GetProjectsAsync();
+            return projects.FirstOrDefault();
+        }
+
+        private async Task<ITeam?> GetProjectTeam(IProject project)
+        {
+            var teams = await _prjClient.GetProjectTeamsAsync(project);
+            return teams.FirstOrDefault();
+        }
+
+
+        [Fact(DisplayName = "Complex test of BoardClient")]
+        public async Task MakeBoardClientTests()
+        {
+            // Arrange
+            IProject? project = null;
+            ITeam? team = null;
+
+            var boards = Enumerable.Empty<IBaseBoard>();
+            IBoard? board = null;
+
+            // Act
+            project = await GetProject();
+            if (project != null)
+            {
+                team = await GetProjectTeam(project);
+                if (team != null)
+                {
+                    boards = await _boardClient.GetBoardsAsync(project.Id, team.Id);
+
+                    if (boards.Any())
+                    {
+                        var baseBoard = boards.First();
+                        board = await _boardClient.GetBoardAsync(project.Id, team.Id, baseBoard.Id);
+                    }
+                }
+            }
+
+            // Assert
+            Assert.NotNull(project);
+            Assert.NotNull(team);
+
+            Assert.True(boards!.Any());
+
+            Assert.NotNull(board);
+            Assert.NotNull(board!.Name);
+            Assert.True(board!.Columns.Any());
+            Assert.True(board!.Rows.Any());
+        }
+    }
+}
